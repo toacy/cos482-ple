83e680bfd623b09186fbc7ade34992e0631bc194
Lucas Chagas <lucascbguimaraes@poli.ufrj.br>
2020-11-05 07:18:40 -0300

    Remoção de arquivos não utilizados no back-end

--- back-end/src/main/java/com/cos482/AcademicLife/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cos482.AcademicLife;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-
-public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
-
-}
--- back-end/src/main/java/com/cos482/AcademicLife/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cos482.AcademicLife;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-
-public interface StudentRepository extends PagingAndSortingRepository<Student, Long> {
-
-}
