5099e6a2718a86c41f3d63d01780cca4d294ea71
= <gabrielxara@gmail.com>
2020-11-05 01:38:44 -0300

    cria rota para login na API

--- back-end/discentes/model.py
+++ back-end/discentes/model.py
@@ -8,16 +8,18 @@ class Discente(Base):
     nome = Column(String(50))
     sobrenome = Column(String(50))
     email = Column(String(120), unique=True)
+    senha = Column(String(120))
     dia_nascimento = Column(Integer)
     mes_nascimento = Column(Integer)
     ano_nascimento = Column(Integer)
     curso = Column(String(100))
 
-    def __init__(self, id, nome, sobrenome, email, dia_nascimento, mes_nascimento, ano_nascimento, curso):
+    def __init__(self, id, nome, sobrenome, email, senha, dia_nascimento, mes_nascimento, ano_nascimento, curso):
         self.id = id
         self.nome = nome
         self.sobrenome = sobrenome
         self.email = email
+        self.senha = senha
         self.dia_nascimento = dia_nascimento
         self.mes_nascimento = mes_nascimento
         self.ano_nascimento = ano_nascimento
